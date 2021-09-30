e4e75545b2153a6a3e21c64db28a40a7ea03c15b
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-09-30 16:22:36 +0100

    hexagon cards

--- pages/services.php
+++ pages/services.php
@@ -1,4 +1,4 @@
-<?php include($_SERVER['DOCUMENT_ROOT'] . "/pair-project/inc/header.php");  ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pair-project/inc/header.php"); ?>
 
 <div class="services-page-header">
   <div class="overlay">
@@ -10,42 +10,76 @@
 
 <div class="container">
   <div class="cards">
-    <div class="card">
-      <h2>VAT</h2>
-      <div class="icon"></div>
-      <div class="card-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel! Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
-        </p>
+    <div class="hex-card">
+      <div class="hex1">
+        <div class="hex2">
+          <h2>Accounts</h2>
+          <div class="icon"></div>
+          <div class="card-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel!
+              Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+            </p>
+          </div>
+        </div>
       </div>
-    </div><div class="card">
-      <h2>VAT</h2>
-      <div class="icon"></div>
-      <div class="card-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel! Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
-        </p>
+    </div>
+
+    <div class="hex-card">
+      <div class="hex1">
+        <div class="hex2">
+          <h2>Bookkeeping</h2>
+          <div class="icon"></div>
+          <div class="card-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel!
+              Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+            </p>
+          </div>
+        </div>
       </div>
-    </div><div class="card">
-      <h2>VAT</h2>
-      <div class="icon"></div>
-      <div class="card-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel! Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
-        </p>
+    </div>
+
+    <div class="hex-card">
+      <div class="hex1">
+        <div class="hex2">
+          <h2>Payroll</h2>
+          <div class="icon"></div>
+          <div class="card-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel!
+              Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+            </p>
+          </div>
+        </div>
       </div>
-    </div><div class="card">
-      <h2>VAT</h2>
-      <div class="icon"></div>
-      <div class="card-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel! Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
-        </p>
+    </div>
+
+    <div class="hex-card">
+      <div class="hex1">
+        <div class="hex2">
+          <h2>VAT</h2>
+          <div class="icon"></div>
+          <div class="card-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel!
+              Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+            </p>
+          </div>
+        </div>
       </div>
-    </div><div class="card">
-      <h2>VAT</h2>
-      <div class="icon"></div>
-      <div class="card-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel! Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
-        </p>
+    </div>
+
+    <div class="hex-card">
+      <div class="hex1">
+        <div class="hex2">
+          <h2>Taxation Services</h2>
+          <div class="icon"></div>
+          <div class="card-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel!
+              Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
+
 </div>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pair-project/inc/footer.php"); ?>
