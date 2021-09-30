7c191308441fc8ff5b7ecc2e884b6da2dffa5211
Alex McKendrick <48360400+Hexagonsun70@users.noreply.github.com>
2021-09-30 09:18:55 +0100

    adjusted files for xampp. prepped db for php form functionality

---
+++ inc/sql/first_row.sql
@@ -0,0 +1,3 @@
+INSERT INTO
+  contact_details (first_name, last_name, email, date)
+  VALUES ('Joe', 'Bloggs', 'email@test.com', '2021-09-29 16:10:01');
--- pages/contact.php
+++ pages/contact.php
@@ -1,3 +1,16 @@
-<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header.php");  ?>
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . "/inc/header.php");
+include_once($_SERVER['DOCUMENT_ROOT'] . "/inc/dbh.php");
+?>
+
+    <div class="container">
+      <form class="contact-form" action="index.html" method="post">
+        <input class="f-name" type="text" name="first-name" value="First Name">
+        <input class="s-name" type="text" name="second-name" value="Second Name ">
+        <input class="email" type="email" name="email" value="Email">
+        <input class="subject" type="text" name="subject" value="Subject">
+        <input class="message" type="text" name="message" value="Message">
+      </form>
+    </div>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer.php"); ?>
