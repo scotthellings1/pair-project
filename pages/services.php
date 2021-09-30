3af77937ec48d8b998037ca97b071331d3d4455b
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-09-30 11:48:08 +0100

    services page header

--- pages/services.php
+++ pages/services.php
@@ -1,7 +1,12 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pair-project/inc/header.php");  ?>
 
 <div class="services-page-header">
-    <p>Accountancy Services</p>
+  <div class="overlay">
+    <div class="content">
+      <h1>Accountancy Services</h1>
+    </div>
+
+  </div>
 </div>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pair-project/inc/footer.php"); ?>
