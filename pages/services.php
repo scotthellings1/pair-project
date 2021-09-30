926859361890e4893f2f181a0936d85b6f71a2ed
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-09-30 12:37:44 +0100

    started cards layout

--- pages/services.php
+++ pages/services.php
@@ -5,8 +5,47 @@
     <div class="content">
       <h1>Accountancy Services</h1>
     </div>
-
   </div>
 </div>
 
+<div class="container">
+  <div class="cards">
+    <div class="card">
+      <h2>VAT</h2>
+      <div class="icon"></div>
+      <div class="card-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel! Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+        </p>
+      </div>
+    </div><div class="card">
+      <h2>VAT</h2>
+      <div class="icon"></div>
+      <div class="card-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel! Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+        </p>
+      </div>
+    </div><div class="card">
+      <h2>VAT</h2>
+      <div class="icon"></div>
+      <div class="card-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel! Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+        </p>
+      </div>
+    </div><div class="card">
+      <h2>VAT</h2>
+      <div class="icon"></div>
+      <div class="card-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel! Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+        </p>
+      </div>
+    </div><div class="card">
+      <h2>VAT</h2>
+      <div class="icon"></div>
+      <div class="card-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel! Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pair-project/inc/footer.php"); ?>
