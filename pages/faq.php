dc038d7c82cc4cbc4e5467e710e5532365f4b23c
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-10-01 15:52:06 +0100

    created style partial added header image

--- pages/faq.php
+++ pages/faq.php
@@ -1,3 +1,11 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pair-project/inc/header.php");  ?>
 
+<div class="faq-page-header">
+    <div class="overlay">
+        <div class="content">
+            <h1>Frequently Asked Questions</h1>
+        </div>
+    </div>
+</div>
+
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pair-project/inc/footer.php"); ?>
