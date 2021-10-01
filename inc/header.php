c57ec758258e14992fce3b7cebdc665d037714ce
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-10-01 16:23:12 +0100

    styled mobile menu

--- inc/header.php
+++ inc/header.php
@@ -52,5 +52,29 @@
             </div>
           </div>
         </div>
+        <div class="mobile-menu">
+          <ul class="mobile-header-links">
+            <li class="mobile-header-link">
+              <a href="/pair-project/pages/services.php">My Services</a>
+            </li>
+            <li class="mobile-header-link">
+              <a href="/pair-project/pages/accountant.php">Switch Accountants</a>
+            </li>
+            <li class="mobile-header-link">
+              <a href="/pair-project/pages/faq.php">FAQ's</a>
+            </li>
+            <li class="mobile-header-link">
+              <a href="/pair-project/pages/contact.php">Contact Us</a>
+            </li>
+            <li class="mobile-header-link"><a href="#">
+                <span>
+                <svg xmlns="http://www.w3.org/2000/svg"
+                     class="phone-icon"
+                     viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                </svg>
+                </span>01953 687077</a></li>
+          </ul>
+        </div>
       </header>
     <!-- header end -->
