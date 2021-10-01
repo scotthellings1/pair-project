9ec798ffabcca406c0a23c91e11b4624214698d4
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-10-01 14:18:48 +0100

    completed layout for hexagon cards on all viewports

--- pages/services.php
+++ pages/services.php
@@ -10,71 +10,86 @@
 
 <div class="container">
   <div class="cards">
-    <div class="hex-card">
-      <div class="hex1">
-        <div class="hex2">
-          <h2>Accounts</h2>
-          <div class="icon"></div>
-          <div class="card-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel!
-              Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
-            </p>
+    <div class="card-1">
+      <div class="hex-card ">
+        <div class="hex1">
+          <div class="hex2">
+            <h2>VAT</h2>
+            <div class="icon"></div>
+            <div class="card-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure
+                sit vel!
+                Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="hex-card">
-      <div class="hex1">
-        <div class="hex2">
-          <h2>Bookkeeping</h2>
-          <div class="icon"></div>
-          <div class="card-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel!
-              Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
-            </p>
+    <div class="card-2">
+      <div class="hex-card ">
+        <div class="hex1">
+          <div class="hex2">
+            <h2>VAT</h2>
+            <div class="icon"></div>
+            <div class="card-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure
+                sit vel!
+                Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="hex-card">
-      <div class="hex1">
-        <div class="hex2">
-          <h2>Payroll</h2>
-          <div class="icon"></div>
-          <div class="card-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel!
-              Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
-            </p>
+    <div class="card-3">
+      <div class="hex-card ">
+        <div class="hex1">
+          <div class="hex2">
+            <h2>VAT</h2>
+            <div class="icon"></div>
+            <div class="card-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure
+                sit vel!
+                Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="hex-card">
-      <div class="hex1">
-        <div class="hex2">
-          <h2>VAT</h2>
-          <div class="icon"></div>
-          <div class="card-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel!
-              Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
-            </p>
+    <div class="card-4">
+      <div class="hex-card ">
+        <div class="hex1">
+          <div class="hex2">
+            <h2>VAT</h2>
+            <div class="icon"></div>
+            <div class="card-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure
+                sit vel!
+                Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="hex-card">
-      <div class="hex1">
-        <div class="hex2">
-          <h2>Taxation Services</h2>
-          <div class="icon"></div>
-          <div class="card-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure sit vel!
-              Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
-            </p>
+    <div class="card-5">
+      <div class="hex-card ">
+        <div class="hex1">
+          <div class="hex2">
+            <h2>VAT</h2>
+            <div class="icon"></div>
+            <div class="card-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum et impedit ipsam iste iure
+                sit vel!
+                Alias cum dolor dolore harum illum magnam nobis omnis, quae, ratione repellat unde!
+              </p>
+            </div>
           </div>
         </div>
       </div>
