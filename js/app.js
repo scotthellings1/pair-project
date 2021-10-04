4d3289bae843432a78134ab2a2b8118ab30d2f59
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-10-04 11:01:08 +0100

    add js to toggle menu when menu button clicked and close menu if viewport wider than 753px

--- js/app.js
+++ js/app.js
@@ -2,5 +2,10 @@ menuButton = document.querySelector('.menu-btn')
 mobileMenu = document.querySelector('.mobile-menu')
 
 menuButton.addEventListener('click', (e) => {
-    mobileMenu.classList.add('active')
+    if (window.innerWidth >= 753) {
+        mobileMenu.classList.remove('active')
+    }
+    mobileMenu.classList.toggle('active')
+
+
 })
