2cd6cf57cec676052fdf4fd946c5bf1ddbd26258
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-10-04 11:55:35 +0100

    added hamburgers animation library

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 menuButton = document.querySelector('.menu-btn')
 mobileMenu = document.querySelector('.mobile-menu')
+hamburger = document.querySelector('.hamburger')
 
 menuButton.addEventListener('click', (e) => {
     if (window.innerWidth >= 753) {
         mobileMenu.classList.remove('active')
     }
     mobileMenu.classList.toggle('active')
-
+    hamburger.classList.toggle('is-active')
 
 })
