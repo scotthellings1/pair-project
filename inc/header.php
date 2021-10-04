6ebe651e7f54bb9f8ef635ada77a54f1b67ddbc7
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-10-04 18:53:07 +0100

    fixed merge conflicts

--- inc/header.php
+++ inc/header.php
@@ -1,22 +1,24 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/pair-project/css/styles.css">
-    <script src="/pair-project/js/app.js" defer></script>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-  integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-  crossorigin=""/>
-    <link rel="icon" href="/pair-project/img/logo-sm.jpeg">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-    <title>Sharon Wray - Accountancy Services</title>
-  </head>
-  <body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/pair-project/css/styles.css">
+  <script src="/pair-project/js/app.js" defer></script>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+        crossorigin=""/>
+  <link rel="icon" href="/pair-project/img/logo-sm.jpeg">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+      href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+      rel="stylesheet">
+  <title>Sharon Wray - Accountancy Services</title>
+</head>
+<body>
 <!-- header start -->
 <header>
   <div class="container">
@@ -35,14 +37,18 @@
           <li class="mobile-header-link">
             <a href="/pair-project/pages/contact.php">Contact Us</a>
           </li>
-                <span>
+          <li class="mobile-header-link">
+            <a href="#">
+          <span>
                 <svg xmlns="http://www.w3.org/2000/svg"
                      class="phone-icon"
                      viewBox="0 0 20 20" fill="currentColor">
                   <path
                       d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
                 </svg>
-                </span>01953 687077</a></li>
+          </span>
+          01953 687077</a>
+          </li>
         </ul>
       </div>
       <div class="logo">
