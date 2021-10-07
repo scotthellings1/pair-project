24b31a7c96a1fd441e028757e4c6cb61c7a08a8f
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-10-07 12:28:50 +0100

    completed privacy policy page

--- pages/privacy.php
+++ pages/privacy.php
@@ -1,3 +1,218 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header.php");  ?>
+<div class="privacy">
+  <h1>Privacy Policy</h1>
 
+  <p>How we use your information </p>
+
+  <p>We are committed to protecting your privacy. This privacy notice tells you what to expect when Sharon Wray collects
+    personal information. It applies to information we collect about: </p>
+  <ul>
+    <li>visitors to our websites;</li>
+    <li>complainants and other individuals in relation to an enquiry;</li>
+    <li>people who use our services; and</li>
+    <li>job applicants and our current and former employees.</li>
+  </ul>
+
+
+  <h3>We use information held about you in the following ways</h3>
+  <ul>
+    <li>to ensure that content from our site is presented in the most effective manner for you and for your computer;
+    </li>
+    <li>to provide you with information, products or services that you request from us or which we feel may interest
+      you,
+      where you have consented to be contacted for such purposes;
+    </li>
+    <li>to carry out our obligations arising from any contracts entered into between you and us;</li>
+    <li>to notify you about changes to our service;</li>
+  </ul>
+  <p>If you are an existing Customer, we will only contact you by electronic means with information about goods and
+    services similar to those which were the subject of a previous sale to you or our customer.</p>
+
+  <p>We do not disclose personal information about individuals to advertisers or sell your information to any other
+    organisation for marketing purposes.</p>
+
+  <h2>Visitors to our websites</h2>
+  <p>When someone visits our site we do not make any attempt to find out the identities of those visiting
+    our website. If we do want to collect personally identifiable information through our website, we will be up front
+    about this. We will make it clear when we collect personal information and will explain what we intend to do with
+    it.</p>
+
+  <h2>Use of cookies by Sharon Wray</h2>
+  <p>You can read more about how we use cookies on our Cookie Policy page.</p>
+
+  <h2>People who email us</h2>
+  <p>Any email sent to us, including any attachments, may be monitored and used by us for reasons of security and for
+    monitoring compliance with office policy. Email monitoring or blocking software may also be used. Please be aware
+    that
+    you have a responsibility to ensure that any email you send to us is within the bounds of the law.</p>
+
+  <h2>People who make a complaint</h2>
+  <p>When we receive a complaint from a person we make up a file containing the details of the complaint. This normally
+    contains the identity of the complainant and any other individuals involved in the complaint.</p>
+
+  <p>We will only use the personal information we collect to process the complaint and to check on the level of service
+    we
+    provide.</p>
+
+  <p>We will keep personal information contained in complaint files in line with our retention policy. It will be
+    retained
+    in a secure environment and access to it will be restricted according to the ‘need to know’ principle.</p>
+
+  <h2>People who enquire with us</h2>
+  <p>Where enquiries are submitted to us we use the information supplied to process the enquiry. When consent is given,
+    we
+    will continue to contact the enquirer with information on services that we provide.</p>
+
+  <p>We may retain personal information, in the form of names and email addresses, for direct marketing
+    communications.</p>
+
+  <p>Individuals can opt out of these communications at anytime by unsubscribing via a link included in emails or
+    contacting us directly using the details at the end of this Policy.</p>
+
+  <p>We will not share any personal data received through this method with 3rd parties.</p>
+
+  <h2>People who use Sharon Wray’s services</h2>
+  <p>Sharon Wray offers various services to the public.</p>
+
+  <p>We have to hold the details of the people who have requested the service in order to provide it. However, we only
+    use
+    these details to provide the service the person has requested and for other closely related purposes. For example,
+    we
+    might use information about people who have subscribed to any of our services to carry out a survey to find out if
+    they are happy with the level of service they received.</p>
+
+  <p>When people do subscribe to our services, they can cancel their subscription at any time and are given an easy way
+    of
+    doing this. We do not store financial details of our clients (credit or debit card numbers).</p>
+
+  <h2>Job applicants, current, and former Organisation employees</h2>
+  <p>When individuals apply to work at Sharon Wray, we will only use the information they supply to us to process their
+    application. Where we want to disclose information to a third party, for example where we want to take up a
+    reference
+    or obtain a ‘disclosure’ from the Criminal Records Bureau we will not do so without informing them beforehand unless
+    the disclosure is required by law.</p>
+
+  <p>Once a person has taken up employment with us, we will compile a file relating to their employment. The information
+    contained in this will be kept secure and will only be used for purposes directly relevant to that person’s
+    employment. Once their employment with us has ended, we will retain the file in accordance with the requirements of
+    our retention schedule and then delete it.</p>
+
+  <h2>Disclosure of personal information</h2>
+  <p>In many circumstances we will not disclose personal data without consent. However when we investigate a complaint,
+    for example, we will need to share personal information with the organisation or individual concerned and with other
+    relevant bodies.</p>
+
+  <p>There are also circumstances where we can pass on personal data without consent for example, to prevent and detect
+    crime and to produce anonymised statistics.</p>
+
+  <h2>Access to personal information</h2>
+  <p>The Organisation tries to be as open as it can be in terms of giving people access to their personal information.
+    Individuals can find out if we hold any personal information by making a ‘subject access request’ under the General
+    Data Protection Regulation. If we do hold information about you we will:</p>
+
+  <ul>
+    <li>give you a description of it;</li>
+    <li>tell you why we are holding it;</li>
+    <li>tell you who it could be disclosed to; and</li>
+    <li>let you have a copy of the information in an intelligible form.</li>
+  </ul>
+  <p>To make a request to Sharon Wray for any personal information we may hold, you need to put the request in writing
+    and
+    either address it to us at the address provided below, or email us.</p>
+
+  <p>If you agree, we will try to deal with your request informally, for example by providing you with the specific
+    information you need over the telephone.</p>
+
+  <p>If we do hold information about you, you can ask us to correct any mistakes by, once again, contacting us.</p>
+
+  <p>As an individual, if you have issues accessing your personal information with any organisation you have the right
+    to
+    lodge a compliant with the ICO.</p>
+
+  <p>Your Rights as a Data Subject:</p>
+
+  <p>Right to Recification – the right to request us to rectify inaccurate personal data.</p>
+
+  <p>Right to Object – the right to object to processing based on either public interests or legitimate interests.
+    Processing must stop, unless we demonstrate compelling grounds for continuing the processing or that the processing
+    is
+    necessary in connection with our legal rights.</p>
+
+  <p>Right to Object to Direct Marketing</p>
+
+  <p>Right to be Forgotten – the right to have us erase personal data without undue delay. Contingent on the occurrence
+    of
+    one of the following: – The data is no longer necessary; – You withdraw your consent (where consent is our legal
+    basis
+    for processing); – We have no overriding grounds for continuing processing against the objectification; – Processing
+    was unlawful; – Erasure is necessary with EU or national law.</p>
+
+  <p>Right to Restrict Processing – the right to have us restrict processing if: – The accuracy of the data is
+    contested;
+    – Processing is unlawful; – We no longer need the data for its original purpose, but need it for legal purposes; –
+    Erasure is pending.</p>
+
+  <p>Right of Data Portability – the right to receive a copy of your data in a commonly used machine-readable format for
+    transfer to another controller. This will either be in .xls or .csv format.</p>
+
+  <p>The Organisation neither makes a decision solely by automated means without any human involvement nor engages in
+    automated processing of personal data to evaluate certain things about an individual. Hence your right related to
+    automated decision making including profiling does not apply with us.</p>
+
+  <p>Any formal subject access request should be made in writing to the address below. This will be provided free of
+    charge. However we may charge a reasonable fee for repetitive, unfounded, or excessive requests or additional
+    copies.</p>
+
+  <h2>Retention & Deletion</h2>
+  <p>Sharon Wray retains your information while you or your company remains an active client, unless you ask us to
+    delete
+    your information. If you stop being an active client we will retain your information for the periods defined in our
+    retention policy unless you request that your details be deleted. However, we will only contact you if we believe
+    the
+    information we intend to send to you could be of ‘legitimate interest’ to you or your company.</p>
+
+  <p>If you have signed up to receive our newsletter or requested information from us, we will retain your information
+    until you request to be removed/deleted. Subject to the exceptions described below, Sharon Wray deletes or
+    anonymizes
+    your information when it’s reached the end of its retention period as per our policy or upon request from you. For
+    further information regarding our retention periods and policy, please contact us via “email[link to email here]” or
+    at the address provided at the end of this policy.</p>
+
+  <p>Subject to applicable law, Sharon Wray may retain information after account deletion: – If there is an unresolved
+    issue relating to your account, such as an outstanding invoice on your account. – If necessary for its legitimate
+    business interests, such as fraud prevention. – If we are required to by applicable law; and/or in aggregated and/or
+    anonymized form.</p>
+
+  <h2>Complaints or queries</h2>
+  <p>Sharon Wray tries to meet the highest standards when collecting and using personal information. For this reason, we
+    take any complaints we receive about this very seriously. We encourage people to bring it to our attention if they
+    think that our collection or use of information is unfair, misleading or inappropriate. We would also welcome any
+    suggestions for improving our procedures.</p>
+
+  <p>This privacy notice was drafted with brevity and clarity in mind. It does not provide exhaustive detail of all
+    aspects of Sharon Wray’s collection and use of personal information. However, we are happy to provide any additional
+    information or explanation needed. Any requests for this should be sent to the address below.</p>
+
+  <h2>Links to other websites</h2>
+  <p>This privacy notice does not cover the links within this site linking to other websites. We encourage you to read
+    the
+    privacy statements on the other websites you visit.</p>
+
+  <h2>Changes to this privacy notice</h2>
+  <p>We keep our privacy notice under regular review. This privacy notice was last updated on 31 May 2018.</p>
+
+  <h2>How to contact us</h2>
+  <p>If you want to request information about our privacy policy you can email us or write to our Data Protection
+    Officer
+    at:</p>
+
+  Sharon Wray <br><br>
+
+  01953 687077<br><br>
+
+  accounts@sharonwray.co.uk<br><br>
+
+  Mobile 07894 067905<br><br>
+
+  Unit 4 Silver Farm, Silver Street, Besthorpe, Norfolk. NR17 2NY2NY</div>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer.php"); ?>
