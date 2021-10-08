46bfe90b9451400db694dae13629431fc8181e25
Alex McKendrick <48360400+Hexagonsun70@users.noreply.github.com>
2021-10-08 10:16:58 +0100

    added t&c page + styling. deleted unwanted script in footer. cleaned up.

--- inc/footer.php
+++ inc/footer.php
@@ -57,8 +57,6 @@
           </ul>
         </div>
       </div>
-
-
     </div>
   </div>
   <div class="line"></div>
@@ -71,12 +69,6 @@
   </div>
 </footer>
 <!-- footer end -->
-
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-crossorigin=""></script>
-
-<script src="/js/leafMap.js"></script>
 
 </body>
 </html>
